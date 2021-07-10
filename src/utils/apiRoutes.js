ed395cacfa56655551b33d8446656dfd7bbd4ef6
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-10 06:20:04 +0530

    updated api routes

--- src/utils/apiRoutes.js
+++ src/utils/apiRoutes.js
@@ -1,6 +1,10 @@
-export const BASEURL =
+export const BASE_URL =
   'https://shoppin-square-server.apurvchimralwar.repl.co/api/';
 
-export const PRODUCTSROUTE = BASEURL.concat('products');
-export const WISHLISTROUTE = BASEURL.concat('wishlist');
-export const CARTROUTE = BASEURL.concat('cart');
+/** PRODUCTS  */
+export const ALL_PRODUCTS = BASE_URL.concat('products/');
+export const WOMENS_PRODUCT = ALL_PRODUCTS.concat('?filter[category]=women');
+export const MENS_PRODUCT = ALL_PRODUCTS.concat('?filter[category]=men');
+
+export const WISHLISTROUTE = BASE_URL.concat('wishlist/');
+export const CARTROUTE = BASE_URL.concat('cart/');

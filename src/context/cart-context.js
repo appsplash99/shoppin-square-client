859fe905ac669c303ca37787bdcf32aeb62a87cc
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-12 05:45:48 +0530

    updated cart context

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -1,27 +1,23 @@
 import { useContext, createContext, useReducer } from 'react';
-// import {
-//   PRODUCTSDATA
-// } from "../productsDB";
 import { cartStateReducer } from '../reducers/cartStateReducer';
-// import { getMaxPrice, getMinPrice } from '../utils/sortAndFilterFunctions';
+import { ALL_PRODUCTS } from '../utils/apiRoutes';
 
 const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const initialState = {
-    // shoppingItems: PRODUCTSDATA,
     shoppingItems: [],
     wishlistItems: [],
     cartItems: [],
     sortBy: null,
     showAllInventory: false,
     showFastDeliveryOnly: false,
-    // priceRangeSlider: {
-    //   status: false,
-    //   min: 0,
-    //   max: 1000,
-    //   value: (getMaxPrice(data) - getMinPrice(data)) / 2
-    // }
+    currentProductsApiRoute: ALL_PRODUCTS,
+    errorMessage: '',
+    toast: {
+      value: false,
+      message: '',
+    },
   };
   const [state, dispatch] = useReducer(cartStateReducer, initialState);
 

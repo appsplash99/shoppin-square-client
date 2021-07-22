bf294dd40076339b642d96e74ea3c0ffe854c0ed
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-22 18:04:52 +0530

    updated sort container

--- src/components/NewSortAndFilter/NewSortContainer.jsx
+++ src/components/NewSortAndFilter/NewSortContainer.jsx
@@ -1,21 +1,50 @@
 import { BtnIcon } from 'morphine-ui';
 import { IoIosCloseCircle } from 'react-icons/io';
 import { useCartState } from '../../context/cart-context';
+import { ALL_PRODUCTS } from '../../utils/apiRoutes';
+import { loadProductsFromDB } from '../../utils/newServerRequests';
 
-export const NewtSortContainer = ({ handleCloseSortContainer }) => {
+export const NewSortContainer = ({ handleCloseSortContainer }) => {
   const {
-    state: { sortBy },
-    dispatch: sorterDispatch,
+    state: { sortBy, currentProductsApiRoute },
+    dispatch,
   } = useCartState();
 
+  const handleOnchange = async (e) => {
+    const sortingValue = e.target.value;
+    let url;
+    if (currentProductsApiRoute === ALL_PRODUCTS) {
+      url = `${currentProductsApiRoute}?sort=${sortingValue}`;
+    } else {
+      url = `${currentProductsApiRoute}&sort=${sortingValue}`;
+    }
+    console.log({ sortingValue, url });
+    const { data } = await loadProductsFromDB(url);
+    // TODO: getting sorted data but unable to add it to state
+    console.log({ data });
+    // dispatch({ type: 'SHOW_LOADER' });
+    // dispatch({ type: 'LOAD_PRODUCTS', payload: data });
+    // dispatch({ type: 'HIDE_LOADER' });
+  };
+
   return (
-    <div className="sort-container">
+    // <div className="sort-container">
+    <div
+      style={{
+        position: 'absolute',
+        zIndex: 'var(--zIndex--overlay)',
+        minWidth: '15rem',
+        backgroundColor: 'var(--themeSecondary)',
+        marginTop: 'var(--space-xxs)',
+        borderRadius: 'var(--space-sm)',
+        left: '-70%',
+      }}>
       <div className="flex flex--row-reverse p">
         <BtnIcon
           size="lg"
           onClick={handleCloseSortContainer}
           style={{ position: 'absolute', top: 0, right: 0 }}>
-          <IoIosCloseCircle className="text--xl" />
+          <IoIosCloseCircle className="text--xl cursor--pointer" />
         </BtnIcon>
         <div className="flex flex--column gap--sm align-items--fs justify-content--c">
           {/* PRODUCTS SORTER */}
@@ -24,11 +53,10 @@ export const NewtSortContainer = ({ handleCloseSortContainer }) => {
               <input
                 type="radio"
                 name="sort"
+                value="price_asc"
                 className="mx--sm radio-btn"
-                onChange={() =>
-                  sorterDispatch({ type: 'SORT', payload: 'PRICE_LOW_TO_HIGH' })
-                }
-                checked={sortBy && sortBy === 'PRICE_LOW_TO_HIGH'}
+                onChange={async (e) => handleOnchange(e)}
+                checked={sortBy}
               />
               Price (Lowest First)
             </label>
@@ -38,14 +66,10 @@ export const NewtSortContainer = ({ handleCloseSortContainer }) => {
               <input
                 type="radio"
                 name="sort"
+                value="price_desc"
                 className="mx--sm radio-btn"
-                onChange={() =>
-                  sorterDispatch({
-                    type: 'SORT',
-                    payload: 'PRICE_HIGHT_TO_LOW',
-                  })
-                }
-                checked={sortBy && sortBy === 'PRICE_HIGHT_TO_LOW'}
+                onChange={async (e) => handleOnchange(e)}
+                checked={sortBy}
               />
               Price (Highest First)
             </label>
@@ -53,40 +77,82 @@ export const NewtSortContainer = ({ handleCloseSortContainer }) => {
           {/* Discount SORTER */}
           <div>
             <label className="text--sm">
-              <input type="radio" name="sort" className="mx--sm radio-btn" />
+              <input
+                type="radio"
+                name="sort"
+                value="discount_asc"
+                className="mx--sm radio-btn"
+                onChange={async (e) => handleOnchange(e)}
+                checked={sortBy}
+              />
               Discount (Lowest First)
             </label>
           </div>
           <div>
             <label className="text--sm">
-              <input type="radio" name="sort" className="mx--sm radio-btn" />
+              <input
+                type="radio"
+                name="sort"
+                value="discount_desc"
+                className="mx--sm radio-btn"
+                onChange={async (e) => handleOnchange(e)}
+                checked={sortBy}
+              />
               Discount (Highest First)
             </label>
           </div>
           {/* Ratings SORTER */}
           <div>
             <label className="text--sm">
-              <input type="radio" name="sort" className="mx--sm radio-btn" />
+              <input
+                type="radio"
+                name="sort"
+                value="ratings_asc"
+                className="mx--sm radio-btn"
+                onChange={async (e) => handleOnchange(e)}
+                checked={sortBy}
+              />
               Ratings (Lowest First)
             </label>
           </div>
           <div>
             <label className="text--sm">
-              <input type="radio" name="sort" className="mx--sm radio-btn" />
+              <input
+                type="radio"
+                name="sort"
+                value="ratings_desc"
+                className="mx--sm radio-btn"
+                onChange={async (e) => handleOnchange(e)}
+                checked={sortBy}
+              />
               Ratings (Highest First)
             </label>
           </div>
           {/* Number of Ratings SORTER */}
           <div>
             <label className="text--sm">
-              <input type="radio" name="sort" className="mx--sm radio-btn" />
-              Least Number of Ratings (Lowest First)
+              <input
+                type="radio"
+                name="sort"
+                className="mx--sm radio-btn"
+                value="number_of_ratings_asc"
+                onChange={async (e) => handleOnchange(e)}
+                checked={sortBy}
+              />
+              Least Number of Ratings
             </label>
           </div>
           <div>
             <label className="text--sm">
-              <input type="radio" name="sort" className="mx--sm radio-btn" />
-              Most Number of Ratings (Highest First)
+              <input
+                type="radio"
+                name="sort"
+                value="number_of_ratings_desc"
+                className="mx--sm radio-btn"
+                onChange={async (e) => handleOnchange(e)}
+                checked={sortBy}
+              />
+              Most Number of Ratings
             </label>
           </div>
         </div>

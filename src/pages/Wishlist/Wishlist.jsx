326e0592fdbe7d3a3ceccb42d342377a41ce1a17
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-24 15:05:47 +0530

    updated wishlist Page

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,44 +1,35 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './Wishlist.css';
-import axios from 'axios';
+import {
+  getWishlistItems,
+  productRemoveFromWishlist,
+} from '../../utils/serverRequests';
 import { Btn, LoaderDonutSpinner } from 'morphine-ui';
-import { WISHLIST_ROUTE } from '../../utils/apiRoutes';
+import { EmptyCartOrWishlist } from '../../components';
 import { useCartState } from '../../context/cart-context';
 import { getLocalCredentials } from '../../utils/localStorage';
-import { productRemoveFromWishlist } from '../../utils/serverRequests';
-import { EmptyCartOrWishlist } from '../../components';
 
 export const Wishlist = () => {
-  const [isLoading, setIsLoading] = useState(false);
   const {
     dispatch,
-    state: { wishlistItems },
+    state: { wishlistItems, showLoader },
   } = useCartState();
 
   useEffect(() => {
-    (async () => {
-      try {
-        setIsLoading(true);
-        const { token, userId } = getLocalCredentials();
-        const newResp = await axios({
-          method: 'GET',
-          url: WISHLIST_ROUTE + `/${userId}`,
-          headers: { Authorization: token },
-        });
-        // console.log(JSON.stringify(newResp, null, 2));
-        dispatch({
-          type: 'LOAD_WISHLIST_ITEMS',
-          payload: newResp.data.userWishlist.wishlistItems,
-        });
-        setIsLoading(false);
-      } catch (error) {
-        setIsLoading(false);
-        console.log(JSON.stringify(error, null, 2));
-      }
-    })();
-  }, [dispatch]);
+    getWishlistItems({ dispatch });
+  }, []);
 
-  if (wishlistItems && wishlistItems.length === 0 && !isLoading) {
+  if (showLoader) {
+    return (
+      <div
+        className="flex align-items--c justify-content--c"
+        style={{ height: '90vh' }}>
+        <LoaderDonutSpinner size="xxl" variant="primary" />
+      </div>
+    );
+  }
+
+  if (wishlistItems && wishlistItems.length === 0 && !showLoader) {
     return (
       <EmptyCartOrWishlist displayText="Your Wishlist is Empty - Please add products" />
     );
@@ -46,71 +37,57 @@ export const Wishlist = () => {
 
   return (
     <div className="wishlist-container">
-      {!wishlistItems ? (
-        <div
-          className="flex align-items--c justify-content--c"
-          style={{
-            height: 'calc(100vh - 8vh)',
-            display: isLoading ? 'flex' : 'none',
-          }}>
-          <LoaderDonutSpinner size="xxl" variant="primary" />
-        </div>
-      ) : (
-        <div
-          className={`checkout-section flex flex--column flex-wrap--wrap gap ${
-            isLoading && 'display--hidden'
-          }`}>
-          <div className="flex flex--column gap border-radius--xs">
-            {wishlistItems.length > 0 &&
-              wishlistItems.map(({ _id, quantity: qty, product }) => {
-                return (
-                  <div
-                    key={_id}
-                    className="bg--secondary"
-                    style={{
-                      marginLeft: 'auto',
-                      marginRight: 'auto',
-                      maxWidth: '28rem',
-                    }}>
-                    <div className="product-cardPosition flex gap">
-                      <img
-                        className="product__image wishlist-product-image"
-                        src={product && product.images[0]}
-                        alt=""
-                        style={{ height: '50%', width: '10rem' }}
-                      />
-                      <div className="product__content flex flex--column gap--xxs p--xxs justify-content--c">
-                        <div className="flex justify-content--sb">
-                          <div className="text--md">
-                            <div className="font-weight--600 flex">
-                              {product.brandName}
-                            </div>
+      <div className="checkout-section flex flex--column flex-wrap--wrap gap">
+        <div className="flex flex--column gap border-radius--xs">
+          {wishlistItems.length > 0 &&
+            wishlistItems.map(({ _id, quantity: qty, product }) => {
+              return (
+                <div
+                  key={_id}
+                  className="bg--secondary"
+                  style={{
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    maxWidth: '28rem',
+                  }}>
+                  <div className="product-cardPosition flex gap">
+                    <img
+                      className="product__image wishlist-product-image"
+                      src={product && product.images[0]}
+                      alt=""
+                      style={{ height: '50%', width: '10rem' }}
+                    />
+                    <div className="product__content flex flex--column gap--xxs p--xxs justify-content--c">
+                      <div className="flex justify-content--sb">
+                        <div className="text--md">
+                          <div className="font-weight--600 flex">
+                            {product.brandName}
                           </div>
                         </div>
-                        <div className="flex align-items--c flex-wrap--wrap">
-                          <Btn
-                            variant="error"
-                            size="xxs"
-                            onClick={async () => {
-                              const { token, userId } = getLocalCredentials();
-                              productRemoveFromWishlist(
-                                dispatch,
-                                token,
-                                userId,
-                                product._id
-                              );
-                            }}>
-                            Remove from Wishlist
-                          </Btn>
-                        </div>
+                      </div>
+                      <div className="flex align-items--c flex-wrap--wrap">
+                        <Btn
+                          variant="error"
+                          size="xxs"
+                          onClick={async () => {
+                            const { token, userId } = getLocalCredentials();
+                            productRemoveFromWishlist(
+                              dispatch,
+                              token,
+                              userId,
+                              product._id
+                            );
+                          }}>
+                          Remove from Wishlist
+                        </Btn>
                       </div>
                     </div>
                   </div>
-                );
-              })}
-          </div>
+                </div>
+              );
+            })}
         </div>
-      )}
+      </div>
     </div>
   );
 };

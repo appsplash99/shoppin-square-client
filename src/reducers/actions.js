266000056bd718d72846a3231f845cd6c0594869
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-24 14:50:41 +0530

    added paginatedActions

--- src/reducers/actions.js
+++ src/reducers/actions.js
@@ -36,6 +36,7 @@ export const loaderActions = {
 
 export const paginationActions = {
   SET_TOTAL_PAGES: 'SET_TOTAL_PAGES',
+  SET_CURRENT_PAGE: 'SET_CURRENT_PAGE',
 };
 
 export const errorActions = { SET_ERROR_MESSAGE: 'SET_ERROR_MESSAGE' };

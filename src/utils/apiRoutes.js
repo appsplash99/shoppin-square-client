431343d05237f35ee3450dc00c0b295ff2ca5b1b
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-25 00:23:25 +0530

    updated productApiMaker

--- src/utils/apiRoutes.js
+++ src/utils/apiRoutes.js
@@ -3,8 +3,6 @@ export const BASE_URL =
 
 /** PRODUCTS  */
 export const ALL_PRODUCTS = BASE_URL + '/products';
-export const WOMENS_PRODUCT = ALL_PRODUCTS + '?filter[category]=women';
-export const MENS_PRODUCT = ALL_PRODUCTS + '?filter[category]=men';
 
 /** PROTECTED CART ROUTES */
 export const CART_ROUTE = BASE_URL + '/cart';
@@ -17,17 +15,26 @@ export const LOGIN_ROUTE = BASE_URL + '/login';
 export const REGISTER_ROUTE = BASE_URL + '/register';
 
 /** API URL Maker */
-export const makeProductsApiUrl = ({ allProductsApi, sortBy, filterObj }) => {
+export const makeProductsApiUrl = ({
+  allProductsApi,
+  sortBy,
+  filterObj,
+  currentPage,
+}) => {
   const activeFilters = Object.keys(filterObj).length > 0;
+
+  let apiUrl = '';
 
   // case 1 -  when sort and filters are empty
   if (!sortBy && !activeFilters) {
-    return allProductsApi;
+    // return allProductsApi;
+    apiUrl = allProductsApi;
   }
 
   // case 2 - When only sortBy is present
   if (sortBy && !activeFilters) {
-    return `${allProductsApi}?sort=${sortBy}`;
+    // return `${allProductsApi}?sort=${sortBy}`;
+    apiUrl = `${allProductsApi}?sort=${sortBy}`;
   }
 
   // case 3 - when only filters are present
@@ -36,7 +43,8 @@ export const makeProductsApiUrl = ({ allProductsApi, sortBy, filterObj }) => {
     const filterString = filterObjKeysArr
       .map((key) => `filter[${key}]=${filterObj[key]}`)
       .join('&');
-    return `${allProductsApi}?${filterString}`;
+    // return `${allProductsApi}?${filterString}`;
+    apiUrl = `${allProductsApi}?${filterString}`;
   }
 
   // case 4 - when both sortBy and filtersObj is present
@@ -45,6 +53,19 @@ export const makeProductsApiUrl = ({ allProductsApi, sortBy, filterObj }) => {
     const filterString = filterObjKeysArr
       .map((key) => `filter[${key}]=${filterObj[key]}`)
       .join('&');
-    return `${allProductsApi}?${filterString}&sort=${sortBy}`;
+    // return `${allProductsApi}?${filterString}&sort=${sortBy}`;
+    apiUrl = `${allProductsApi}?${filterString}&sort=${sortBy}`;
   }
+
+  // after all above cases add currentpage to apiUrl
+  // if currentPage is present
+  if (currentPage) {
+    if (apiUrl.includes('?')) {
+      return `${apiUrl}&page=${currentPage}`;
+    } else {
+      return `${apiUrl}?page=${currentPage}`;
+    }
+  }
+
+  return apiUrl;
 };

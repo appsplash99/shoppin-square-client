f2f7b4c57306e090ff559d8e4697967b73abe285
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-24 22:51:35 +0530

    added Api route maker

--- src/utils/apiRoutes.js
+++ src/utils/apiRoutes.js
@@ -15,3 +15,36 @@ export const WISHLIST_ROUTE = BASE_URL + '/wishlist';
 /**LOGIN AND REGISTER */
 export const LOGIN_ROUTE = BASE_URL + '/login';
 export const REGISTER_ROUTE = BASE_URL + '/register';
+
+/** API URL Maker */
+export const makeProductsApiUrl = ({ allProductsApi, sortBy, filterObj }) => {
+  const activeFilters = Object.keys(filterObj).length > 0;
+
+  // case 1 -  when sort and filters are empty
+  if (!sortBy && !activeFilters) {
+    return allProductsApi;
+  }
+
+  // case 2 - When only sortBy is present
+  if (sortBy && !activeFilters) {
+    return `${allProductsApi}?sort=${sortBy}`;
+  }
+
+  // case 3 - when only filters are present
+  if (!sortBy && activeFilters) {
+    const filterObjKeysArr = Object.keys(filterObj);
+    const filterString = filterObjKeysArr
+      .map((key) => `filter[${key}]=${filterObj[key]}`)
+      .join('&');
+    return `${allProductsApi}?${filterString}`;
+  }
+
+  // case 4 - when both sortBy and filtersObj is present
+  if (sortBy && activeFilters) {
+    const filterObjKeysArr = Object.keys(filterObj);
+    const filterString = filterObjKeysArr
+      .map((key) => `filter[${key}]=${filterObj[key]}`)
+      .join('&');
+    return `${allProductsApi}?${filterString}&sort=${sortBy}`;
+  }
+};

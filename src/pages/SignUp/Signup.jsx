13491eece9ca4c56a79444730b311916b85d3237
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-24 14:45:05 +0530

    finalised signup page

--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Btn } from 'morphine-ui';
+import { useNavigate } from 'react-router-dom';
+import { Btn, LoaderDonutSpinner } from 'morphine-ui';
+import { signUpUser } from '../../utils/serverRequests';
+import { useCartState } from '../../context/cart-context';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { signupValidationSchema } from '../../utils/formValidations';
-import axios from 'axios';
-import { REGISTER_ROUTE } from '../../utils/apiRoutes';
-import { useNavigate } from 'react-router-dom';
+import { IoIosCloseCircle } from 'react-icons/io';
 
-export const Signup = (props) => {
+export const Signup = () => {
+  const {
+    dispatch,
+    state: { showLoader, errorMessage },
+  } = useCartState();
   const navigate = useNavigate();
-
   const signupPageStyle = {
     margin: 'var(--space-md) auto',
     maxWidth: 'calc(4*var(--space-xxl))',
@@ -27,90 +31,95 @@ export const Signup = (props) => {
 
   // formik's handle submit
   const onSubmit = async (values) => {
-    console.log('Form data', values);
-
-    // Signup New User
-    try {
-      const { name, email, password } = values;
-      await axios({
-        method: 'post',
-        url: `${REGISTER_ROUTE}`,
-        data: { name, email, password },
-      });
-      alert('Signin Successful - Please Login');
-      navigate('/login');
-      /** Navigate to User Cart after successfull signup */
-      // console.log(REGISTER_ROUTE);
-      // console.log({ response }, null, 2);
-    } catch (error) {
-      console.log(error);
-    }
+    signUpUser({ dispatch, navigate, userData: values });
   };
 
   return (
-    <>
-      <div className="container ">
-        <div
-          className="login-wrapper flex flex--column gap--md align-items--c"
-          style={signupPageStyle}>
-          <h2>Sign Up</h2>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={signupValidationSchema}
-            onSubmit={onSubmit}>
-            <Form className="mt-1 w--50% flex flex--column align-items--c justify-content--c gap--sm">
-              {/* User Name */}
-              <div className="form-group">
-                <Field
-                  type="text"
-                  name="name"
-                  className="form-control border-radius--sm p--xs"
-                  placeholder="Name"
-                />
-                <ErrorMessage
-                  name="name"
-                  component="p"
-                  className="text--danger"
-                />
-              </div>
-              {/* EMAIL */}
-              <div className="form-group">
-                <Field
-                  type="text"
-                  name="email"
-                  className="form-control border-radius--sm p--xs"
-                  placeholder="Email"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="p"
-                  className="text--danger"
-                />
-              </div>
-              <div className="form-group">
-                <Field
-                  type="password"
-                  name="password"
-                  className="form-control border-radius--sm p--xs"
-                  placeholder="Password"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="p"
-                  className="text--danger p--xxs "
-                />
-              </div>
+    <div className="container ">
+      <div
+        className="login-wrapper flex flex--column gap--md align-items--c"
+        style={signupPageStyle}>
+        <h2>Sign Up</h2>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={signupValidationSchema}
+          onSubmit={onSubmit}>
+          <Form className="mt-1 w--50% flex flex--column align-items--c justify-content--c gap--sm">
+            <div
+              className={`text--danger p--sm ${
+                errorMessage === '' && 'display--none'
+              } `}
+              style={{ position: 'relative' }}>
+              <IoIosCloseCircle
+                className={`${
+                  errorMessage === '' && 'display--hidden'
+                } text--lg text--dark cursor--pointer`}
+                style={{
+                  position: 'absolute',
+                  right: 'var(--space-xxs)',
+                  top: 'var(--space-xxs)',
+                }}
+                onClick={() => {
+                  dispatch({ type: 'SET_ERROR_MESSAGE', payload: '' });
+                }}
+              />
+              {errorMessage}
+            </div>
+            {/* User Name */}
+            <div className="form-group">
+              <Field
+                type="text"
+                name="name"
+                className="form-control border-radius--sm p--xs"
+                placeholder="Name"
+              />
+              <ErrorMessage
+                name="name"
+                component="p"
+                className="text--danger"
+              />
+            </div>
+            {/* EMAIL */}
+            <div className="form-group">
+              <Field
+                type="text"
+                name="email"
+                className="form-control border-radius--sm p--xs"
+                placeholder="Email"
+              />
+              <ErrorMessage
+                name="email"
+                component="p"
+                className="text--danger"
+              />
+            </div>
+            <div className="form-group">
+              <Field
+                type="password"
+                name="password"
+                className="form-control border-radius--sm p--xs"
+                placeholder="Password"
+              />
+              <ErrorMessage
+                name="password"
+                component="p"
+                className="text--danger p--xxs "
+              />
+            </div>
+            {showLoader ? (
+              <LoaderDonutSpinner size="lg" variant="primary" />
+            ) : (
               <Btn
+                size="sm"
                 type="submit"
-                size="xs"
                 variant="primary"
-                className="border-radius--sm  w--100% p--0 p--xxs--xs">
-                signup
+                className="border-radius--sm w--70%">
+                Sign Up
               </Btn>
-            </Form>
-          </Formik>
-        </div>
+            )}
+          </Form>
+        </Formik>
       </div>
-    </>
+    </div>
   );
 };

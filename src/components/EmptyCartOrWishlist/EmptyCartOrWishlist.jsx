11bc4a9f3b46ccee08cdf67c65d9d2f8e271b65d
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-24 15:43:36 +0530

    updated onclick for SHOP Products

--- src/components/EmptyCartOrWishlist/EmptyCartOrWishlist.jsx
+++ src/components/EmptyCartOrWishlist/EmptyCartOrWishlist.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Btn } from 'morphine-ui';
+import { Link } from 'react-router-dom';
+import { ALL_PRODUCTS } from '../../utils/apiRoutes';
+import { useCartState } from '../../context/cart-context';
 
 export const EmptyCartOrWishlist = ({ displayText }) => {
+  const { dispatch } = useCartState();
   return (
     <div
       className="mt--lg pb--lg text--dark flex flex--column justify-content--c align-items--c gap"
       style={{ height: '50vh' }}>
       <div>{displayText}</div>
-      <Link to="/shop">
+      <Link
+        to="/shop"
+        onClick={() => {
+          dispatch({
+            type: 'CHANGE_PRODUCT_CATEGORY',
+            payload: { route: ALL_PRODUCTS },
+          });
+          // console.log({ currentProductsApiRoute });
+        }}>
         <Btn variant="primary" shape="capsule" size="sm">
           Shop Products
         </Btn>

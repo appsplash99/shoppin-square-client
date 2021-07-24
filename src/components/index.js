f4e7e4fa77bdac09145dbec65f340c1b6c72299a
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-24 22:45:28 +0530

    updated sort filter components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import { PrivateRoute } from './PrivateRoute';
 import { ChangeProductQtyBtn } from './Buttons';
 import { ResponsiveNavbar } from './ResponsiveNavbar/ResponsiveNavbar';
-import { NewSortAndFilter } from './NewSortAndFilter/NewSortAndFilter';
 import { Sort } from './Sort/Sort';
 import { Filter } from './Filter/Filter';
 import { EmptyCartOrWishlist } from './EmptyCartOrWishlist/EmptyCartOrWishlist';
@@ -10,7 +9,6 @@ export {
   Sort,
   Filter,
   PrivateRoute,
-  NewSortAndFilter,
   ResponsiveNavbar,
   ChangeProductQtyBtn,
   EmptyCartOrWishlist,

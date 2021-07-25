cb335e0594351dcbd71181bcc34b266a98876c0f
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-26 00:22:37 +0530

    updated Product Detail

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './ProductDetail.css';
-import { FaStar } from 'react-icons/fa';
+import { FaStar, FaShippingFast } from 'react-icons/fa';
 import { BiRupee } from 'react-icons/bi';
+import { MdEventAvailable } from 'react-icons/md';
 import { useParams } from 'react-router-dom';
 import ReactImageMagnify from 'react-image-magnify';
 import { ALL_PRODUCTS } from '../../utils/apiRoutes';
 import { LoaderDonutSpinner } from 'morphine-ui';
+import { AddToCartBtn } from './AddToCartBtn';
+import { AddToWishlistBtn } from './AddToWIshlistBtn';
 
 export const ProductDetail = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -21,6 +23,7 @@ export const ProductDetail = () => {
 
   useEffect(() => {
     (async function () {
+      setIsLoading(true);
       const {
         data: { success, product },
       } = await axios.get(`${ALL_PRODUCTS}/${productId}`);
@@ -35,86 +38,107 @@ export const ProductDetail = () => {
     })();
   }, [productId]);
 
+  if (isLoading) {
+    return (
+      <div
+        className="flex align-items--c justify-content--c"
+        style={{ height: '92vh' }}>
+        <LoaderDonutSpinner size="xxl" variant="primary" />
+      </div>
+    );
+  }
+
   return (
-    <div className="product">
-      {isLoading ? (
-        <div
-          className="flex align-items--c justify-content--c"
-          style={{
-            height: 'calc(100vh - 8vh)',
-          }}>
-          <LoaderDonutSpinner size="xxl" variant="primary" />
-        </div>
-      ) : (
-        <>
-          <div className="product--detail">
-            <div className="image--array">
-              {desiredProduct &&
-                desiredProduct?.images &&
-                desiredProduct?.images.map((item) => (
-                  <img onClick={() => setCurrImage(item)} src={item} alt="" />
-                ))}
-            </div>
-            <div className="product--image">
-              <ReactImageMagnify
-                enlargedImagePosition="over"
-                {...{
-                  smallImage: {
-                    alt: desiredProduct?.brandName,
-                    isFluidWidth: true,
-                    src: currImage,
-                  },
-                  largeImage: {
-                    src: currImage,
-                    width: '100%',
-                    height: '100%',
-                  },
+    <div className="flex flex--column align-items--c justify-content--c gap--xxl mt--xl">
+      <div className="flex gap">
+        <div className="flex flex--column align-items--c justify-content--fs gap mx--sm">
+          {desiredProduct &&
+            desiredProduct?.images &&
+            desiredProduct?.images.map((item) => (
+              <img
+                className="cursor--pointer"
+                style={{
+                  height: 'var(--space-xxl)',
+                  width: 'var(--space-xxl)',
                 }}
+                onClick={() => setCurrImage(item)}
+                src={item}
+                alt={desiredProduct?.brandName}
               />
+            ))}
+        </div>
+        <div style={{ display: 'flex', width: '50%', height: 'auto' }}>
+          <ReactImageMagnify
+            enlargedImagePosition="over"
+            style={{ width: '100%', height: 'auto' }}
+            {...{
+              smallImage: {
+                alt: desiredProduct?.brandName,
+                isFluidWidth: true,
+                src: currImage,
+                width: '4rem',
+                height: '6rem',
+              },
+              largeImage: {
+                src: currImage,
+                width: 1200,
+                height: 1800,
+              },
+            }}
+          />
+        </div>
+        <div className="flex flex--column align-items--fs justify-self--fs w--70% gap--xs">
+          <h1 className="font-weight--600">{desiredProduct?.brandName}</h1>
+          <p className="text-align--l">{desiredProduct?.description}</p>
+          {desiredProduct?.fastDelivery && (
+            <div className="flex align-items--c justify-self--fs gap--sm">
+              <FaShippingFast className="text--primary text--xl" />
+              <p>Fast Delivery Eligible</p>
             </div>
-            <div className="product--details">
-              <h1 className="product--name">{desiredProduct?.brandName}</h1>
-              {desiredProduct?.price && (
-                <div className="product--price">
-                  <p className="new--price">
-                    <BiRupee color={'#000'} />
-                    {desiredProduct?.price}
-                  </p>
-                  <p className="old--price">
-                    <BiRupee color={'#000'} />
-                    {Math.round(
-                      (desiredProduct?.price * 100) /
-                        (100 - desiredProduct?.price)
-                    )}
-                  </p>
-                  <p className="discount">{desiredProduct?.discount}</p>
-                </div>
-              )}
-              {desiredProduct?.inStock}
-              <div className="product--description">
-                {desiredProduct?.description}
-              </div>
-              <p>{desiredProduct?.fastDelivery && 'Available'}</p>
-              {desiredProduct?.ratings && (
-                <div className="align-self--fs text--sm font-weight--500 flex align-items--c justify-content--c gap--xs">
-                  {desiredProduct?.ratings}
-                  <FaStar className="text--warning align-self--c text--sm" />
-                  <span className="text--sm ">|</span>
-                  {desiredProduct?.numberOfRatings}
-                </div>
-              )}
+          )}
+          {desiredProduct?.isNewProduct && (
+            <div className="flex align-items--c justify-self--fs gap--sm">
+              <MdEventAvailable className="text--primary text--xl" />
+              <p>Newest roduct</p>
             </div>
+          )}
+          {desiredProduct?.inStock && (
+            <div className="flex align-items--c justify-self--fs gap--sm">
+              <MdEventAvailable className="text--primary text--xl" />
+              <p>Item in Stock</p>
+            </div>
+          )}
+          {desiredProduct?.price && (
+            <div className="flex justify-content--sa gap--sm">
+              <p className="text--lg font-weight--600">
+                <BiRupee className="text--dark" />
+                {desiredProduct?.price}
+              </p>
+              <p className="text--xs font-weight--600 text--strikethrough">
+                <BiRupee className="text--dark" />
+                {Math.round(
+                  (desiredProduct?.price * 100) / (100 - desiredProduct?.price)
+                )}
+              </p>
+              <p className="bg--danger text--light p--xxs">
+                {desiredProduct?.discount}% Off
+              </p>
+            </div>
+          )}
+          {desiredProduct?.ratings && (
+            <div className="align-self--fs text--sm font-weight--500 flex align-items--c justify-content--c gap--xs">
+              {desiredProduct?.ratings}
+              <FaStar className="text--warning align-self--c text--sm" />
+              <span className="text--sm ">|</span>
+              {desiredProduct?.numberOfRatings}
+            </div>
+          )}
+          <div className="flex align-items--c gap--xs">
+            <AddToCartBtn desiredProduct={desiredProduct} />
+            <AddToWishlistBtn desiredProduct={desiredProduct} />
           </div>
-          <div className="card-btn">
-            {/* <div className="addToCartBtn">
-              {inStock && <BiCart size={32} onClick={addToCart} />}
-            </div>
-            <div className="wish" onClick={handleWish}>
-              {!isWish() ? <BsHeart size={26} /> : <BsHeartFill size={26} />}
-            </div> */}
-          </div>
-        </>
-      )}
+        </div>
+      </div>
     </div>
   );
 };

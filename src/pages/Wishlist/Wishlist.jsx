3eb1bc27326b70cdf369fed8f728d3e704731d36
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-28 11:40:28 +0530

    updated wishlist card

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -52,6 +52,8 @@ export const Wishlist = () => {
                   marginLeft: 'auto',
                   marginRight: 'auto',
                   maxWidth: '28rem',
+                  boxShadow:
+                    'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
                 }}>
                 <div
                   className="product-cardPosition flex gap p--sm"

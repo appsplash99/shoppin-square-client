1223ed63c3aeb86cefed5dbe5abfba6aaf2fd95d
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-28 06:56:44 +0530

    updated with SET ProductsPerPage reducer case

--- src/reducers/cartStateReducer.js
+++ src/reducers/cartStateReducer.js
@@ -19,6 +19,7 @@ import { makeProductsApiUrl, ALL_PRODUCTS } from '../utils/apiRoutes';
 export const cartStateReducer = (prevState, action) => {
   switch (action.type) {
     case filterActions.CLEAR_ALL_FILTERS:
+      // console.log('Dispatched from CLEAR_ALL_FILTERS');
       return {
         ...prevState,
         filterObj: {},
@@ -30,6 +31,7 @@ export const cartStateReducer = (prevState, action) => {
       };
 
     case filterActions.FILTER_CATEGORY: {
+      // console.log('Dispatched from FILTER_CATEGORY');
       let newFilterObj = {};
       if (action.payload === 'all-products') {
         delete prevState.filterObj.category;
@@ -53,6 +55,8 @@ export const cartStateReducer = (prevState, action) => {
     }
 
     case filterActions.FILTER_IS_NEW_PRODUCT: {
+      // console.log('Dispatched from FILTER_IS_NEW_PRODUCT');
+
       let newFilterObj = {};
       if (action.payload === true) {
         newFilterObj = { ...prevState.filterObj, is_new_product: true };
@@ -71,11 +75,12 @@ export const cartStateReducer = (prevState, action) => {
           sortBy: prevState.sortBy,
           filterObj: newFilterObj,
         }),
-        // pagination: {...prevState.pagination}
       };
     }
 
     case filterActions.FILTER_IN_STOCK: {
+      // console.log('Dispatched from FILTER_IN_STOCK');
+
       let newFilterObj = {};
       if (action.payload === true) {
         newFilterObj = { ...prevState.filterObj, in_stock: true };
@@ -98,6 +103,8 @@ export const cartStateReducer = (prevState, action) => {
     }
 
     case filterActions.FILTER_SALE_ITEM: {
+      // console.log('Dispatched from FILTER_SALE_ITEM');
+
       let newFilterObj = {};
       if (action.payload === true) {
         newFilterObj = { ...prevState.filterObj, sale: true };
@@ -119,6 +126,8 @@ export const cartStateReducer = (prevState, action) => {
     }
 
     case filterActions.FILTER_FAST_DELIVERY: {
+      // console.log('Dispatched from FILTER_FAST_DELIVERY');
+
       let newFilterObj = {};
       if (action.payload === true) {
         newFilterObj = { ...prevState.filterObj, fast_delivery: true };
@@ -140,6 +149,7 @@ export const cartStateReducer = (prevState, action) => {
     }
 
     case sortActions.SORT:
+      // console.log('Dispatched from SORT');
       return {
         ...prevState,
         // update context
@@ -153,6 +163,7 @@ export const cartStateReducer = (prevState, action) => {
       };
 
     case paginationActions.SET_TOTAL_PAGES: {
+      // console.log('Dispatched from SET_TOTAL_PAGES');
       return {
         ...prevState,
         pagination: {
@@ -163,6 +174,7 @@ export const cartStateReducer = (prevState, action) => {
     }
 
     case paginationActions.SET_CURRENT_PAGE: {
+      // console.log('Dispatched from SET_CURRENT_PAGE');
       return {
         ...prevState,
         pagination: {
@@ -176,17 +188,48 @@ export const cartStateReducer = (prevState, action) => {
           sortBy: prevState.sortBy,
           // use Latest value to make api Route
           currentPage: action.payload,
+          productsPerPage: prevState.pagination.productsPerPage,
         }),
       };
     }
 
+    case paginationActions.SET_PRODUCTS_PER_PAGE: {
+      // console.log('Dispatched from SET_PRODUCTS_PER_PAGE');
+
+      return {
+        ...prevState,
+        pagination: {
+          ...prevState.pagination,
+          productsPerPage: action.payload,
+        },
+        currentProductsApiRoute: makeProductsApiUrl({
+          allProductsApi: ALL_PRODUCTS,
+          sortBy: prevState.sortBy,
+          filterObj: { ...prevState.filterObj },
+          productsPerPage: action.payload,
+        }),
+
+        // // TODO: remove after done
+        // // CHECKING STATE
+        // checkingState: {
+        //   ...prevState.checkingState,
+        //   fromSetProductsPerPage:
+        //     prevState.checkingState.fromSetProductsPerPage + 1,
+        // },
+      };
+    }
+
     case loaderActions.SHOW_LOADER:
+      // console.log('Dispatched from SHOW_LOADER');
+
       return { ...prevState, showLoader: true };
 
     case loaderActions.HIDE_LOADER:
+      // console.log('Dispatched from HIDE_LOADER');
       return { ...prevState, showLoader: false };
 
     case userActions.LOG_OUT_USER:
+      // console.log('Dispatched from LOG_OUT_USER');
       return {
         ...prevState,
         shoppingItems: [],
@@ -195,28 +238,45 @@ export const cartStateReducer = (prevState, action) => {
       };
 
     case errorActions.SET_ERROR_MESSAGE:
+      // console.log('Dispatched from SET_ERROR_MESSAGE');
       return { ...prevState, errorMessage: action.payload };
 
     case shoppingProductsAction.LOAD_PRODUCTS:
+      // console.log('Dispatched from LOAD_PRODUCTS');
       return {
         ...prevState,
         pagination: {
           ...prevState.pagination,
           totalPages: action.payload.totalPages,
+          // Added Later
+          productsPerPage: action.payload.limit,
         },
         shoppingItems: action.payload.products,
+
+        // // TODO: remove after done
+        // // CHECKING STATE
+        // checkingState: {
+        //   ...prevState.checkingState,
+        //   fromLoadProducts: prevState.checkingState.fromLoadProducts + 1,
+        // },
       };
 
     case cartActions.LOAD_CART_ITEMS:
+      // console.log('Dispatched from LOAD_CART_ITEMS');
+
       return { ...prevState, cartItems: action.payload };
 
     case cartActions.SET_CART_TOTAL:
+      // console.log('Dispatched from SET_CART_TOTAL');
+
       return { ...prevState, cartTotal: action.payload };
 
     case wishlistActions.LOAD_WISHLIST_ITEMS:
+      // console.log('Dispatched from LOAD_WISHLIST_ITEMS');
       return { ...prevState, wishlistItems: action.payload };
 
     case wishlistActions.ADD_OR_REMOVE_FROM_WISHLIST:
+      // console.log('Dispatched from ADD_OR_REMOVE_FROM_WISHLIST');
       return {
         ...prevState,
         wishlistItems: !isProductInArray(
